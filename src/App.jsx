c207e9c2c5a4d5a9643281342750caf18c9dc7fa
AhkrorWDev's <167975242+Ahrorbek05@users.noreply.github.com>
2025-01-14 17:31:39 +0500

    Revert "Yangi branch orqali o'zgartirishlarni push qilish"

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Contact from "./pages/Contact";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import Coursereg from "./pages/Coursereg";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/course" element={<Course />} />
-      <Route path="/coursereg" element={<Coursereg />} />
       <Route path="/buy" element={<Payment />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/register" element={<Register />} />
